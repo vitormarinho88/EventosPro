cbb31b92219c1ee1e1474d05b9d605cba303f809
Vitor Marinho <120474483+vitormarinho88@users.noreply.github.com>
2023-11-20 15:30:48 -0300

    Update eventos.component.ts

--- Front/ProEventos-app/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-app/src/app/eventos/eventos.component.ts
@@ -54,7 +54,7 @@ export class EventosComponent implements OnInit {
 
 
   public getEventos(): void {
-      this.http.get('https://localhost:5001/api/eventos').subscribe(
+      this.http.get('https://localhost:44327/api/eventos').subscribe(
 
         response =>
        {this.eventos = response;
